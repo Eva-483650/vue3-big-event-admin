a14227e950bc4eefd1082667dd3f550c67fab1c9
Eva <3396796974@qq.com>
2025-07-15 14:23:38 +0800

    初始化提交测试

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// 1.获取路由对象 router useRouter
+// 2.获取路由参数 route useRoute
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+const goList = () => {
+  // 路由跳转
+  router.push('/list')
+  console.log('goList', route)
+}
+</script>
+
+<template>
+  <div>
+    我是app
+    <button @click="goList">jump to list</button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// 初始化 vue-router4.x(vue3)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: []
+})
+
+export default router
